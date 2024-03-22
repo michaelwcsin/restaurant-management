2a8c264381e3fc671d1b3d61481830cff978c5b9
plysnh <plysnh@gmail.com>
2024-03-22 13:25:04 -0600

    add card to restaurants

---
+++ frontend/src/Restaurant.js
@@ -0,0 +1,98 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import NavBar from "./components/shared/navbar/navbar.component";
+import MenuList from "./components/user/menu/menuList.component";
+import { Container, Card, Image, Dimmer, Header, Input, Button } from 'semantic-ui-react';
+
+
+function RestaurantList() {
+  const [restaurants, setRestaurants] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/restaurants");
+        console.log("Response:", response.data);
+        setRestaurants(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching restaurants:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchRestaurants();
+  }, []);
+
+  const filteredRestaurants = restaurants.filter((restaurant) =>
+    restaurant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+
+  return (
+
+      <div className="restaurant-list">
+          <NavBar/>
+          <Container style={{padding: 20}}>
+              <Header as="h1">Restaurants List</Header>
+
+              <div className="search-bar">
+                  <Input
+                      focus
+                      type="text"
+                      placeholder="Search Restaurant 🔍"
+                      value={searchTerm}
+                      onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+              </div>
+
+              {loading ? (
+                  <div>Loading...</div>
+              ) : filteredRestaurants.length === 0 ? (
+                  <p>No restaurants found.</p>
+              ) : (
+
+                  <div className="restaurant-container">
+                      {filteredRestaurants.map((restaurant) => (
+                          <Card key={restaurant._id}>
+                            <Dimmer.Dimmable
+                                as={Image}
+                                dimmed={active}
+                                dimmer={{ active, content: (
+                                    <Button primary onClick={() => alert('Place your order!')}>
+                                        Order Here
+                                    </Button>
+                                ) }}
+                                onMouseEnter={() => setActive(true)}
+                                onMouseLeave={() => setActive(false)}
+                                // src='/assets/restaurantsphoto/Cactus.jpg'
+                                src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg'
+                            />
+                            <Card.Content>
+                                <Card.Header>{restaurant.name}</Card.Header>
+                                <Card.Meta>Email: {restaurant.email}</Card.Meta>
+                                <Card.Description>Address: {restaurant.address}</Card.Description>
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Link to={`/restaurants/${restaurant._id}/details`}>
+                                    Place Order Here
+                                </Link>
+                            </Card.Content>
+                        </Card>
+                      ))}
+                  </div>
+              )}
+
+              <MenuList/>
+
+          </Container>
+      </div>
+  )}
+
+
+
+export default RestaurantList;
