072ffd9d71de5a9a49af4a2f0710092a7cc23015
israjime <israhjime@gmail.com>
2024-03-22 04:51:08 -0600

    New design homepage

--- frontend/src/Components/shared/navbar/navbar.component.jsx
+++ frontend/src/Components/shared/navbar/navbar.component.jsx
@@ -1,15 +1,31 @@
 import Container from "react-bootstrap/Container";
+import "./navbar.styles.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavLink from "react-bootstrap/esm/NavLink";
 
 function NavBar() {
   return (
-    <Navbar bg="dark" data-bs-theme="dark">
-      <Container>
-        <Navbar.Brand href="/home">Home</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/restaurants">Restaurant</Nav.Link>
-          <Nav.Link href="/customers">User</Nav.Link>
+    <Navbar>
+      <Container className="navBar">
+        <Nav>
+          <Navbar.Brand>
+            <img
+              alt=""
+              src="favicon.ico"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            <b1>MINK</b1>
+          </Navbar.Brand>
+        </Nav>
+        <Nav>
+          <NavLink href="/home"><b>Home</b></NavLink>
+          <div class="vr"></div>
+          <Nav.Link href="/restaurants"><b>Restaurant</b></Nav.Link>
+          <div class="vr"></div>
+          <Nav.Link href="/customers"><b>Customer</b></Nav.Link>
         </Nav>
       </Container>
     </Navbar>
--- frontend/src/pages/shared/homepage.pages.js
+++ frontend/src/pages/shared/homepage.pages.js
@@ -1,23 +1,26 @@
 import React from "react";
 import NavBar from "../../components/shared/navbar/navbar.component";
 import "./homepage.styles.css";
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <NavBar />
+      <Stack className="onboarding">
+        <h1>MINK</h1>
+        <h3>Food Service Made Easy</h3>
+        <Stack className="twoButtons">
+          <Button variant="light" className="shadow-sm">
+            <a href="/customers"><b>Customer View</b></a>
+          </Button>
 
-      <div className="onboarding">
-        <h1>MINKs</h1>
-
-        <button>
-          <a href="/customers">Go to Customer View</a>
-        </button>
-
-        <button>
-          <a href="/restaurants">Go to Manager view</a>
-        </button>
-      </div>
+          <Button variant="light" className="shadow-sm">
+            <a href="/restaurants"><b>Manager view</b></a>
+          </Button>
+        </Stack>
+      </Stack>
     </div>
   );
 };
