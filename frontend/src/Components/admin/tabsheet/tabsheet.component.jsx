b1c7f4e64894b46e1c050ea7231903de32b5e210
sinm4 <sinm4@mymacewan.ca>
2024-03-27 09:25:22 -0600

    render menu list for restaurant

--- frontend/src/Components/admin/tabsheet/tabsheet.component.jsx
+++ frontend/src/Components/admin/tabsheet/tabsheet.component.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { Tab, TabPane } from "semantic-ui-react";
+import MenuList from "../../user/menu/menuList.component";
 import "./tabsheet.styles.css";
 
 const panes = [
-  { menuItem: "Menu Items", render: () => <TabPane>Tab 1 Content</TabPane> },
+  {
+    menuItem: "Menu Items",
+    render: () => (
+      <TabPane>
+        <MenuList />
+      </TabPane>
+    ),
+  },
   { menuItem: "Orders", render: () => <TabPane>Tab 2 Content</TabPane> },
   { menuItem: "Analytics", render: () => <TabPane>Tab 3 Content</TabPane> },
 ];
