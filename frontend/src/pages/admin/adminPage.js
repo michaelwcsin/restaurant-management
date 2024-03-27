2b4a6da2186d73109f8331ce0f2f39b8b5b3571b
sinm4 <sinm4@mymacewan.ca>
2024-03-27 08:48:33 -0600

    TODO for admin page

--- frontend/src/pages/admin/adminPage.js
+++ frontend/src/pages/admin/adminPage.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { useRestaurant } from "../../components/admin/restaurants/RestaurantContext";
-import NavBar from "../../components/restaurant/navbar/restaurantNavBar.component";
+
+// TODO: Given this page, add components of menu list for specific restaurant -> alter status of menu item
+// TODO: Add - add/remove menu item to this page
+// TODO: Check orders
+// TODO: Analytics
 
 const AdminPage = () => {
   const { selectedRestaurant } = useRestaurant();
 
   return (
     <div>
-      <NavBar />
-      <p>Hello</p>
+      <p>{selectedRestaurant._id}</p>
       <p>{selectedRestaurant.name}</p>
       <p>{selectedRestaurant.address}</p>
       <p>{selectedRestaurant.phone}</p>
-      <p>Goodbyes</p>
     </div>
   );
 };
