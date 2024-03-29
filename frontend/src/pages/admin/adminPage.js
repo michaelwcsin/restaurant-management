666501c39243ecc469e70507232fb082258cd757
sinm4 <sinm4@mymacewan.ca>
2024-03-29 09:07:05 -0600

    Added functionality to grab menu from restaurant ID (just goes through all menu items and checks if id is contained

--- frontend/src/pages/admin/adminPage.js
+++ frontend/src/pages/admin/adminPage.js
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useRestaurant } from "../../components/admin/restaurants/RestaurantContext";
 import TabSheet from "../../components/admin/tabsheet/tabsheet.component";
 import "./adminPage.styles.css";
@@ -10,12 +11,34 @@ import "./adminPage.styles.css";
 
 const AdminPage = () => {
   const { selectedRestaurant } = useRestaurant();
+  const [menus, setMenus] = useState([]);
+
+  useEffect(() => {
+    const fetchMenus = async () => {
+      const response = await axios("http://localhost:8000/menus");
+      setMenus(response.data);
+    };
+    fetchMenus();
+  }, []);
+
+  const filteredMenus = menus.filter((menu) =>
+    selectedRestaurant.menuItems.includes(menu._id)
+  );
 
   return (
     <div className="admin-sheet">
       <h2>{selectedRestaurant.name}</h2>
       <h4>{selectedRestaurant.address}</h4>
       <h4>{selectedRestaurant.phone}</h4>
+      <h4>{selectedRestaurant._id}</h4>
+      <h4>{selectedRestaurant.menuItems}</h4>
+      <ul>
+        {filteredMenus.map((menu) => (
+          <li key={menu._id}>
+            <strong>{menu.name}</strong> - {menu.description} - ${menu.price}
+          </li>
+        ))}
+      </ul>
       <TabSheet />
       {/* <p>{selectedRestaurant._id}</p> */}
     </div>
