e9239f648c84fba2c54fb870718657b5b7e842c4
sinm4 <sinm4@mymacewan.ca>
2024-03-30 15:31:26 -0600

    Change to pull menu item from restaurant menu

--- frontend/src/Components/user/menu/menuList.component.jsx
+++ frontend/src/Components/user/menu/menuList.component.jsx
@@ -41,7 +41,7 @@ const MenuList = ({ restaurant }) => {
   return (
     <div className="menu-list">
       {filteredMenuItems.map((menu) => (
-        <MenuItem key={menu._id} menuItem={menu} />
+        <MenuItem key={menu._id} menuItem={menu} restaurant={restaurant} />
       ))}
     </div>
   );
