bfbd2ba8af4e55956310547274df8556e2edcbcb
plysnh <plysnh@gmail.com>
2024-04-04 10:33:22 -0600

    Update customer.model.js

--- backend/models/customer.model.js
+++ backend/models/customer.model.js
@@ -4,6 +4,7 @@ const CustomerSchema = new mongoose.Schema({
   //only email and password are required
   name: { type: String, required: false },
   email: { type: String, required: true },
+  password: { type: String, required: true },
   address: { type: String, required: false },
   phone: { type: String, required: false },
 });
